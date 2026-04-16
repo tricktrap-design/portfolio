d47bdc0b73e9575b7955c4def0772e07cc1294a2
Emilio Arboleya <trickbook-air@Emilios-MacBook-Air.local>
2026-04-15 23:57:20 -0600

    Set up Github Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
   build: {
     outDir: "web",
     emptyOutDir: true,
   },
 });
-
